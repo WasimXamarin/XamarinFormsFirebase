fb3b200cb3a73dcfbdc220365589eef3ac56ef5c
WASIM <wasimalam@virtualemployee.com>
2022-01-18 18:49:41 +0530

    Xamarin Forms Firebase
    Xamarin Forms Firebase New Project Add

---
+++ XamarinFormsFirebase/XamarinFormsFirebase/XamarinFormsFirebase/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinFormsFirebase.ViewModels;
+
+namespace XamarinFormsFirebase.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
